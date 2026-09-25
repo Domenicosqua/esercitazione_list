85a98d4f5de9756f50bbfd8c24a7bf3e171efc1f
domenico squadrito <domenico.squadritosp@gmail.com>
2026-09-25 16:04:42 +0200

    aggiunta esercitazione map

--- bin/collection_oop.dart
+++ bin/collection_oop.dart
@@ -48,6 +48,16 @@ void main(List<String> arguments) {
   vgs.remove('fallout');
   print(vgs);
 
+  //* map
+
+  Map<String, String> libri3 = {
+    'Jr Tolkien': 'Il signore degli anelli',
+    'Orwell': 'La fattoria degli animali',
+  };
+
+  print(libri3);
+  // libri3.forEach((autore, titolo) => print('Autore: $autore, Titolo: $titolo'));
+
   //! ESERCITAZIONE
   List<String> libriEsercitazione = [
     'Delitto e Castigo',
@@ -64,4 +74,30 @@ void main(List<String> arguments) {
       .where((vg) => vg.contains('coraggiosi'))
       .toList();
   print(filtroCoraggiosi);
+
+  //! esercitazione map
+
+  Map<String, String> libri4 = {
+    'London': 'Zanna bianca',
+    'Stevenson': 'L\' isola del tesoro',
+    'Salgari': 'Le tigri di Mompracem',
+  };
+  print(libri4);
+
+  libri4.forEach((autore, titolo) => print('Autore: $autore, Titolo: $titolo'));
+
+  libri4["Orwell"] = "1984";
+  //libri4.addAll({'Orwell': '1984'});
+  libri4['London'] = 'il richiamo della foresta';
+  print(libri4);
+  if (libri4.containsKey('Orwell')) {
+    print('Autore Presente');
+  } else {
+    print('autore non presente');
+  }
+  if (libri4.containsValue('1984')) {
+    print('Libro presente');
+  } else {
+    print('libro non presente in archivio');
+  }
 }
